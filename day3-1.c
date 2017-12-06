b0533ced8370c9dd27894c53fd8c05d9196c94cc
Alvaro Perez-Diaz <alvpdiaz@gmail.com>
2017-12-06 15:18:55 +0000

    Solved day3_1

---
+++ day3-1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int squareNumber(int input) {
+  // Compute which square the input belongs to: square n has 8n numbers
+  int Sn, n = 1;
+  while (1) {
+    Sn = 4*n*(n+1) + 1;
+    if (input <= Sn) {
+      break;
+    }
+    n++;
+  }
+  return n;
+}
+
+int manhattanDistance(int input, int n) {
+  int squareSize, pos, groupSize, posGroup, start, mh;
+  squareSize = 8*n;
+  pos = input - 4*n*(n-1) - 2;
+  groupSize = 2*n;
+  posGroup = pos%groupSize;
+  start = 2*n-1;
+  // Compute solution distance
+  if (posGroup < groupSize/2) {
+    mh = start - posGroup;
+  } else {
+    mh = start - groupSize/2 + 1 + (posGroup - groupSize/2 + 1);
+  }
+}
+
+int main(void) {
+  int n, mh, input = 277678;
+  n = squareNumber(input);
+  mh = manhattanDistance(input, n);
+
+  printf("mh=%d", mh);
+  return 0;
+}
