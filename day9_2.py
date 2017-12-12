8609a35910ada865549cc08b763ff7e66ce2a157
Alvaro Perez-Diaz <alvpdiaz@gmail.com>
2017-12-12 16:55:51 +0000

    Solved day 9

---
+++ day9_2.py
@@ -0,0 +1,57 @@
+
+# coding: utf-8
+
+# In[55]:
+
+data = "{{<a!>},{<a!>},{<a!>},{<ab>}}"
+
+
+# In[61]:
+
+filename = "day9.txt"
+with open(filename) as f:
+    data = f.readlines()[0].strip()
+
+
+# In[62]:
+
+def process(data):
+    dataClean = ""
+    # Delete things after "!"s
+    i = 0
+    while i < len(data):
+        if data[i] == "!":
+            pass
+            i += 2
+        else:
+            dataClean += data[i]
+            i += 1
+    data = dataClean
+
+    # Delete things inside "<", ">" pairs
+    deleted = 0
+    oldData = ""
+    while oldData is not data:
+        oldData = data
+        try:
+            start = data.index("<")
+            end = data.index(">")
+        except:
+            break
+        dataL = list(data)[:start] + list(data)[end+1:]
+        data = "".join(dataL)
+        # Keep track of the number of deleted items
+        deleted += (end - start - 1)
+    
+    # Clean all ","s
+    data = data.replace(",", "")
+    return data, deleted
+
+data, deleted = process(data)
+print("Deleted = ", deleted)
+
+
+# In[ ]:
+
+
+
