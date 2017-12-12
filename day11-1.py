6490eb4c556fe03d465a348ad084ba27c4c433ff
Alvaro Perez-Diaz <alvpdiaz@gmail.com>
2017-12-12 19:20:59 +0000

    Solved day11-1

---
+++ day11-1.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+with open("day11.txt") as f:
+    data = f.readline().strip().split(",")
+
+# Delta coordinates
+deltas = {"n": np.array([1, -1, 0]),
+          "s": np.array([-1, 1, 0]),
+          "nw": np.array([1, 0, -1]),
+          "ne": np.array([0, -1, 1]),
+          "sw": np.array([0, 1, -1]),
+          "se": np.array([-1, 0, 1])}
+
+# Starting position
+pos = np.array([0, 0, 0])
+
+# Move
+for move in data:
+    pos += deltas[move]
+print("Result =", max(np.abs(pos)))
