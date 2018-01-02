aa17f72ddfa02973974554be0412273ccf90367c
Alvaro Perez <alvpdiaz@gmail.com>
2018-01-02 19:25:30 +0100

    Solved day 6

---
+++ 2016/day6-2.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+
+with open("day6.txt") as f:
+    data = [x.strip() for x in f.readlines()]
+
+final = []
+for i in range(len(data[0])):
+    mssg = [line[i] for line in data]
+    c = Counter(mssg)
+    final.append(c.most_common(100)[-1][0])
+
+print("Result =", "".join(final))
+
