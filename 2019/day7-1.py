4a5bdb14a6a4224b7fa9cb597b9d5c44f0872c63
Alvaro Perez <alvpdiaz@gmail.com>
2019-12-10 21:22:43 +0100

    improved day7-1

--- 2019/day7-1.py
+++ 2019/day7-1.py
@@ -1,118 +1,110 @@
 from itertools import permutations
+
 import numpy as np
 
 
-def process_opcode_and_param(number):
-    param = str(number)
-    opcode = int(param[-2:])
-    mode = param[:-2]
-    mode = "0" * (3 - len(mode)) + mode
-    return opcode, mode[::-1]
+class Processor:
+    def __init__(self, data: np.array, inputs: list):
+        self.data = data
+        self.inputs = inputs
+        self.outputs = []
+        self.pos = 0
+        self.running = 1
 
+    @staticmethod
+    def process_opcode_and_param(number):
+        param = str(number)
+        opcode = int(param[-2:])
+        mode = param[:-2]
+        mode = "0" * (3 - len(mode)) + mode
+        return opcode, mode[::-1]
 
-def get_value(data, mode, pos):
-    if mode == "0":
-        return data[data[pos]]
-    if mode == "1":
-        return data[pos]
+    def get_value(self, mode, delta_pos):
+        if mode == "0":
+            return self.data[self.data[self.pos + delta_pos]]
+        if mode == "1":
+            return self.data[self.pos + delta_pos]
 
+    def go(self):
+        opcode, mode = self.process_opcode_and_param(self.data[self.pos])
 
-def go(data, pos, last_input):
-    delete = False
-    output = None
+        if opcode == 1:
+            self.data[self.data[self.pos + 3]] = self.get_value(
+                mode[0], 1
+            ) + self.get_value(mode[1], 2)
+            self.pos += 4
 
-    opcode, mode = process_opcode_and_param(data[pos])
+        elif opcode == 2:
+            self.data[self.data[self.pos + 3]] = self.get_value(
+                mode[0], 1
+            ) * self.get_value(mode[1], 2)
+            self.pos += 4
 
-    if opcode == 1:
-        data[data[pos + 3]] = get_value(data, mode[0], pos + 1) + get_value(
-            data, mode[1], pos + 2
-        )
-        pos += 4
+        elif opcode == 3:
+            if len(self.inputs) == 0:
+                self.running = 0
+            else:
+                next_input = self.inputs.pop()
+                self.data[self.data[self.pos + 1]] = next_input
+                self.pos += 2
 
-    elif opcode == 2:
-        data[data[pos + 3]] = get_value(data, mode[0], pos + 1) * get_value(
-            data, mode[1], pos + 2
-        )
-        pos += 4
+        elif opcode == 4:
+            self.outputs.append(self.get_value(mode[0], 1))
+            self.pos += 2
 
-    elif opcode == 3:
-        data[data[pos + 1]] = last_input
-        delete = True
-        pos += 2
+        elif opcode == 5:
+            if self.get_value(mode[0], 1) != 0:
+                self.pos = self.get_value(mode[1], 2)
+            else:
+                self.pos += 3
 
-    elif opcode == 4:
-        output = get_value(data, mode[0], pos + 1)
-        pos += 2
+        elif opcode == 6:
+            if self.get_value(mode[0], 1) == 0:
+                self.pos = self.get_value(mode[1], 2)
+            else:
+                self.pos += 3
 
-    elif opcode == 5:
-        if get_value(data, mode[0], pos + 1) != 0:
-            pos = get_value(data, mode[1], pos + 2)
+        elif opcode == 7:
+            value = 1 if self.get_value(mode[0], 1) < self.get_value(mode[1], 2) else 0
+            self.data[self.data[self.pos + 3]] = value
+            self.pos += 4
+
+        elif opcode == 8:
+            value = 1 if self.get_value(mode[0], 1) == self.get_value(mode[1], 2) else 0
+            self.data[self.data[self.pos + 3]] = value
+            self.pos += 4
+
+        elif opcode == 99:
+            self.pos = -1
+
         else:
-            pos += 3
-
-    elif opcode == 6:
-        if get_value(data, mode[0], pos + 1) == 0:
-            pos = get_value(data, mode[1], pos + 2)
-        else:
-            pos += 3
-
-    elif opcode == 7:
-        value = (
-            1
-            if get_value(data, mode[0], pos + 1) < get_value(data, mode[1], pos + 2)
-            else 0
-        )
-        data[data[pos + 3]] = value
-        pos += 4
-
-    elif opcode == 8:
-        value = (
-            1
-            if get_value(data, mode[0], pos + 1) == get_value(data, mode[1], pos + 2)
-            else 0
-        )
-        data[data[pos + 3]] = value
-        pos += 4
-
-    elif opcode == 99:
-        pos = -1
-
-    else:
-        raise ValueError
-
-    return data, pos, delete, output
+            raise ValueError
 
 
-def solve_amplifier(data, phase, last_input):
+def solve_amplifier(data, phase, first_input):
+    inputs = [first_input, phase]
+    amplifier = Processor(data, inputs)
 
-    inputs = [last_input, phase]
+    while amplifier.pos >= 0:
+        amplifier.go()
 
-    last_output = None
-    pos = 0
-    while pos >= 0:
-        data, pos, delete, output = go(data, pos, inputs[-1])
-        if output:
-            last_output = output
-        if delete and len(inputs) == 2:
-            inputs.pop()
-
-    return last_output
+    return amplifier.outputs[0]
 
 
-def solve(data_base, phases):
-    data = data_base.copy()
-    _input = 0
+def solve_all(data, phases):
+    data_copy = data.copy()
+    next_input = 0
     for phase in phases:
-        _input = solve_amplifier(data, phase, _input)
-    return _input
+        next_input = solve_amplifier(data_copy, phase, next_input)
+    return next_input
 
 
-data_base = np.genfromtxt("day7.txt", delimiter=",", dtype=int)
+if __name__ == "__main__":
+    data = np.genfromtxt("day7.txt", delimiter=",", dtype=int)
 
-max_result = -float("Inf")
-for phases in list(permutations(range(5))):
-    result = solve(data_base, phases)
-    if result > max_result:
-        max_result = result
+    max_result = -float("Inf")
+    for phases in list(permutations(range(5))):
+        result = solve_all(data, phases)
+        max_result = max(result, max_result)
 
-print(max_result)
+    print(max_result)
