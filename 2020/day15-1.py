475cf4333946f90b91ce9471a6cab87c8daf55f2
Alvaro Perez <alvpdiaz@gmail.com>
2020-12-15 19:12:14 +0000

    solved 2020/15

---
+++ 2020/day15-1.py
@@ -0,0 +1,25 @@
+from collections import defaultdict
+
+
+def solve(numbers, n_iter):
+    number_idx = defaultdict(list)
+    [number_idx[number].append(idx) for idx, number in enumerate(numbers)]
+
+    idx = len(numbers)
+    last_number = numbers[-1]
+    while idx < n_iter:
+        if len(number_idx[last_number]) < 2:
+            last_number = 0
+        else:
+            last_number = number_idx[last_number][-1] - number_idx[last_number][-2]
+            number_idx[last_number]
+        number_idx[last_number].append(idx)
+        idx += 1
+
+    return last_number
+
+
+with open("day15.txt", "r") as f:
+    numbers = [int(number) for number in f.read().strip().split(",")]
+
+print(solve(numbers, 2020))
