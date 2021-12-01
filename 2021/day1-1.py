05119c8919846a4764d2581304877faefb8076fa
Alvaro Perez <alvaro.perez@senseye.io>
2021-12-01 13:33:50 +0100

    black

--- 2021/day1-1.py
+++ 2021/day1-1.py
@@ -5,4 +5,4 @@ with open("day1.txt", "r") as f:
     data = [int(line.strip()) for line in f.readlines()]
 
 print(f"Part 1: {sum(np.diff(data) > 0)}")
-print(f"Part 2: {sum(np.diff(np.convolve(data, np.ones(3,dtype=int), 'valid')) > 0)}")
+print(f"Part 2: {sum(np.diff(np.convolve(data, np.ones(3, dtype=int), 'valid')) > 0)}")
