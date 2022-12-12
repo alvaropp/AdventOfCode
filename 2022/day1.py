dc5563c0fd3174d801fdad104eee17cf5ccf5e1c
Alvaro Perez <alvaro.perez@senseye.io>
2022-12-12 16:51:03 +0100

    solved 2022/01

---
+++ 2022/day1.py
@@ -0,0 +1,16 @@
+calories = []
+
+current_calories = 0
+
+for line in open("day1.txt"):
+    if line == "\n":
+        calories.append(current_calories)
+        current_calories = 0
+    else:
+        current_calories += int(line)
+
+calories = sorted(calories, reverse=True)
+
+print(f"Part one: {calories[0]}")
+print(f"Part two: {sum(calories[:2])}")
+
